a19c37bf63f64a62a8ef4cb1d091f8875052adf0
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:13:17 -0300

    Create MaiorPosicao.js

---
+++ DesafioIniciais/MaiorPosicao.js
@@ -0,0 +1,20 @@
+using System; 
+
+class TESTE {
+  static void Main(string[] args) { 
+    int n;
+    int maior = 0;
+    int posicao = 0;
+    
+    for (int i = 1; i <= 100 ; i++) {
+      n = Convert.ToInt32(Console.ReadLine());
+      
+      if (n > maior) {
+        maior = n;
+        posicao = i;
+      } 
+    }
+    Console.WriteLine(maior);
+    Console.WriteLine(posicao);
+  }
+}

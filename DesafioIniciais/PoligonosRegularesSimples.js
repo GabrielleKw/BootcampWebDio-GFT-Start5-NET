2ba5c2a1f0169d371683b683f9af68a1d4aa1578
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:11:20 -0300

    Create PoligonosRegularesSimples.js

---
+++ DesafioIniciais/PoligonosRegularesSimples.js
@@ -0,0 +1,9 @@
+using System; 
+
+class Problem {
+  static void Main(string[] args) { 
+    string[] v = Console.ReadLine().Split();
+        
+    Console.WriteLine(int.Parse(v[0]) * int.Parse(v[1]));
+  }
+}
